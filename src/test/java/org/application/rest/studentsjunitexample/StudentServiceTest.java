7fcc6cd6478101fe15d8c2983e7c68f92974f3b0
kare <karelysperezs@gmail.com>
2025-05-06 17:30:29 +0200

    STUDENT01:add tests for StudentService

--- src/test/java/org/application/rest/studentsjunitexample/StudentServiceTest.java
+++ src/test/java/org/application/rest/studentsjunitexample/StudentServiceTest.java
@@ -17,6 +17,46 @@ class StudentServiceTest {
         boolean actualResult = listOfStudents.isEmpty();
 
         assertTrue(actualResult);
+        assertTrue(()-> actualResult);
+        assertTrue(actualResult, "The list of students should be empty");
+    }
+
+    @Test
+    public void addStudentTest(){
+        StudentService studentService = new StudentService();
+        Student student = new Student(1, "John Doe");
+
+        studentService.addStudent(student);
+
+        List<Student> listOfStudents = studentService.getStudents();
+
+        boolean actualResult = listOfStudents.contains(student);
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void getStudentByIdTest(){
+        StudentService studentService = new StudentService();
+        Student student = new Student(1, "John Doe");
+
+        studentService.addStudent(student);
+
+        int actualResult = student.getId();
+
+        assertEquals(1, actualResult);
+    }
+
+    @Test
+    public void getStudentByNameTest(){
+        StudentService studentService = new StudentService();
+        Student student = new Student(1, "John Doe");
+
+        studentService.addStudent(student);
+
+        String actualResult = student.getName();
+
+        assertEquals(student.getName(), actualResult);
     }
 
 }
