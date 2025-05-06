26ce441db28df8cbb496f0ac908c7864eaf80a7e
kare <karelysperezs@gmail.com>
2025-05-06 13:13:31 +0200

    initialize Spring Boot application with basic structure and tests

---
+++ src/main/java/org/application/rest/studentsjunitexample/StudentsJunitExampleApplication.java
@@ -0,0 +1,13 @@
+package org.application.rest.studentsjunitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsJunitExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsJunitExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/application/rest/studentsjunitexample/StudentsJunitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.rest.studentsjunitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsJunitExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
